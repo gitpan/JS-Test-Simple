59000acbad6189f1ea4a471a5c30001445c89367
Ingy döt Net <ingy@cpan.org>
2008-08-28 06:40:22 +0000

    Import of INGY/JS-Test-Simple-0.28 from CPAN.
    gitpan-cpan-distribution: JS-Test-Simple
    gitpan-cpan-version:      0.28
    gitpan-cpan-path:         INGY/JS-Test-Simple-0.28.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- tests/lib/Test/Builder.js
+++ tests/lib/Test/Builder.js
@@ -1,4 +1,4 @@
-// $Id: /mirror/openjsan/users/theory/Test.Simple/trunk/lib/Test/Builder.js 1950 2008-08-20T06:40:51.750920Z ingy  $
+// $Id: /mirror/openjsan/users/theory/Test.Simple/trunk/lib/Test/Builder.js 2136 2008-08-22T22:17:42.999815Z theory  $
 
 /*global JSAN, Test, WScript, _global */
 
@@ -36,7 +36,7 @@ Test.Builder.globalScope = typeof JSAN != 'undefined'
       ? _global
       : this;
 
-Test.Builder.VERSION = '0.27';
+Test.Builder.VERSION = '0.28';
 Test.Builder.Instances = [];
 Test.Builder.lineEndingRx = /\r?\n|\r/g;
 Test.Builder.StringOps = {
@@ -279,6 +279,7 @@ Test.Builder.prototype.ok = function (test, desc) {
     this._print(out);
 
     if (!test) {
+        // Add URL and line numer using code from http://pastie.org/253058?
         var msg = todo ? "Failed (TODO)" : "Failed";
         this.diag("    " + msg + " test");
     }
